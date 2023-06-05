05bb3061bc1aad23f783547d07979df16378d59a
Rene Regensbogen <68396806+Durus-R@users.noreply.github.com>
2023-06-05 21:13:19 +0200

    change design and layout

--- src/components/MainText.vue
+++ src/components/MainText.vue
@@ -1,9 +1,30 @@
 <template>
-  <h1>{{ props.text }}</h1>
+  <div class="q-mx-auto" @click="visibility = !visibility">
+    <h4 v-if="props.sunday !== undefined">{{ props.sunday }}</h4>
+    <h3 v-if="visibility">
+      {{ props.at_source }}
+    </h3>
+    <h1 v-if="visibility">
+      {{ props.at_text }}
+    </h1>
+    <h3 v-if="!visibility">
+      {{ props.nt_source }}
+    </h3>
+    <h1 v-if="!visibility">
+      {{ props.nt_text }}
+    </h1>
+  </div>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue';
+
+const visibility = ref(true);
 const props = defineProps({
-  text: String,
+  at_text: String,
+  at_source: String,
+  nt_text: String,
+  nt_source: String,
+  sunday: String,
 });
 </script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,12 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <MainText :text="at_source"></MainText>
+  <q-page class="row items-center justify-center dark-background">
+    <MainText
+      :at_text="at_text"
+      :at_source="at_source"
+      :nt_text="nt_text"
+      :nt_source="nt_source"
+      :sunday="sunday"
+    ></MainText>
   </q-page>
 </template>
 
@@ -10,14 +16,22 @@ import MainText from '../components/MainText.vue';
 import { get_today, entry } from 'src/lib/data';
 import Papa from 'papaparse';
 
+const at_text = ref('');
 const at_source = ref('');
+const nt_text = ref('');
+const nt_source = ref('');
+const sunday = ref('');
 
 Papa.parse('/Losungen.csv', {
   encoding: 'CP1252',
   download: true,
   complete: (result) => {
     const jsonData = result.data as entry[];
+    at_text.value = get_today(jsonData)?.at_text ?? '';
     at_source.value = get_today(jsonData)?.at_source ?? '';
+    nt_text.value = get_today(jsonData)?.nt_text ?? '';
+    nt_source.value = get_today(jsonData)?.nt_source ?? '';
+    sunday.value = get_today(jsonData)?.sunday ?? '';
   },
   header: true,
   delimiter: '\t',
