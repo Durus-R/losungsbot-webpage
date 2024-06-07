86214eca79d1e9fd0eebcdab5f3e2e539ff28157
Rene Regensbogen <68396806+Durus-R@users.noreply.github.com>
2024-06-07 13:49:40 +0200

    various ui changes

--- src/components/ActionButtons.vue
+++ src/components/ActionButtons.vue
@@ -1,61 +1,84 @@
 <template>
-  <div v-if="!($q.platform.is.mobile && $q.fullscreen.isActive)">
-    <QBtn round size="lg" v-if="$q.platform.is.mobile" icon="fullscreen" class="fullscreen-btn" @click="$q.fullscreen.request()"></QBtn>
-    <QBtn
-      @click="handleCalendarclick"
-      size="lg"
-      round
-      icon="event"
-      class="calendar-btn"
-      ><QPopupProxy cover transition-show="scale" transition-hide="scale"
-        ><q-date
-          :navigation-max-year-month="max_date"
-          :navigation-min-year-month="min_date"
-          v-model="proxyDate"
-          today-btn
-          dark
-          color="orange"
-          :locale="ger_Locale"
+  <div>
+    <transition-group
+      appear
+      enter-active-class="animated slower backInUp"
+      leave-active-class="animated slower backOutDown"
+    >
+      <QBtn v-if="props.toggleButtons"
+        key="calendar-btn"
+        @click="handleCalendarclick"
+        size="lg"
+        round
+        icon="event"
+        class="calendar-btn"
+      >
+        <QPopupProxy cover transition-show="scale" transition-hide="scale"
         >
-          <div class="row items-center justify-end q-gutter-sm">
-            <q-btn label="Cancel" color="primary" flat v-close-popup />
-            <q-btn
-              label="OK"
-              color="primary"
-              flat
-              @click="handleCalendarclick"
-              v-close-popup
-            />
-          </div> </q-date></QPopupProxy
-    ></QBtn>
-    <QBtn :href="hyperlink" size="lg" round icon="book" class="bibleserver-btn">
-    </QBtn
-    ><QIcon
-      name="expand_less"
-      size="lg"
-      class="icon-up"
-      v-if="$q.platform.is.mobile && store.at"
-    ></QIcon>
-    <QIcon
-      name="expand_less"
-      size="lg"
-      class="icon-up animated-icon-up"
-      color="grey"
-      v-if="$q.platform.is.mobile && store.at"
-    ></QIcon>
-    <QIcon
-      name="expand_more"
-      size="lg"
-      class="icon-down"
-      v-if="$q.platform.is.mobile && !store.at"
-    ></QIcon>
-    <QIcon
-      name="expand_more"
-      size="lg"
-      class="icon-down animated-icon-down"
-      color="grey"
-      v-if="$q.platform.is.mobile && !store.at"
-    ></QIcon>
+          <q-date
+            :navigation-max-year-month="max_date"
+            :navigation-min-year-month="min_date"
+            v-model="proxyDate"
+            today-btn
+            dark
+            color="orange"
+            :locale="ger_Locale"
+          >
+            <div class="row items-center justify-end q-gutter-sm">
+              <q-btn label="Cancel" color="primary" flat v-close-popup />
+              <q-btn
+                label="OK"
+                color="primary"
+                flat
+                @click="handleCalendarclick"
+                v-close-popup
+              />
+            </div>
+          </q-date>
+        </QPopupProxy
+        >
+      </QBtn>
+      <QBtn v-if="props.toggleButtons" key="bibleserver-btn" :href="hyperlink" size="lg" round icon="book" class="bibleserver-btn">
+      </QBtn
+      >
+    </transition-group
+    >
+    <transition-group
+      appear
+      enter-active-class="animated backInUp"
+      leave-active-class="animated fast backOutDown"
+    >
+      <QIcon
+        key="icon-up"
+        name="expand_less"
+        size="lg"
+        class="icon-up"
+        v-if="$q.platform.is.mobile && store.at && props.toggleButtons"
+      ></QIcon>
+      <QIcon
+        key="icon-up-animated"
+        name="expand_less"
+        size="lg"
+        class="icon-up animated-icon-up"
+        color="grey"
+        v-if="$q.platform.is.mobile && store.at && props.toggleButtons"
+      ></QIcon>
+      <QIcon
+        key="icon-down"
+        name="expand_more"
+        size="lg"
+        class="icon-down"
+        v-if="$q.platform.is.mobile && !store.at && props.toggleButtons"
+      ></QIcon>
+      <QIcon
+        key="icon-down-animated"
+        name="expand_more"
+        size="lg"
+        class="icon-down animated-icon-down"
+        color="grey"
+        v-if="$q.platform.is.mobile && !store.at && props.toggleButtons"
+      ></QIcon>
+    </transition-group>
   </div>
 </template>
 
@@ -70,6 +93,7 @@ const $q = useQuasar();
 const props = defineProps({
   at_source: String,
   nt_source: String,
+  toggleButtons: Boolean
 });
 
 const max_date = `${new Date().getFullYear()}/12`;
@@ -93,7 +117,7 @@ const hyperlink = computed(() => {
 });
 watch(hyperlink, () => {
   store.$patch({
-    url: hyperlink.value,
+    url: hyperlink.value
   });
 });
 
@@ -110,6 +134,6 @@ const ger_Locale = {
   monthsShort: 'Jan_Feb_Mär_Apr_Mai_Jun_Jul_Aug_Sep_Okt_Dez'.split('_'),
   firstDayOfWeek: 1, // 0-6, 0 - Sunday, 1 Monday, ...
   format24h: true,
-  pluralDay: 'Tage',
+  pluralDay: 'Tage'
 };
 </script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -4,6 +4,7 @@
     v-touch-swipe.mouse.up="switchNT"
     v-touch-swipe.mouse.down="switchAT"
     @click="handleClick"
+    @mousemove="handleMousemove"
   >
     <MainText
       :at_text="at_text"
@@ -11,8 +12,10 @@
       :nt_text="nt_text"
       :nt_source="nt_source"
     ></MainText>
+    <div class="flex flex-grow flex-column justify-end items-end">
+      <p v-if="(secondsSinceMove < 5) && !($q.platform.is.mobile)">(Klicken, um zwischem Losungsvers und Lehrvers zu wechseln)</p></div>
   </q-page>
-  <ContextButton :at_source="at_source" :nt_source="nt_source"></ContextButton>
+  <ContextButton :toggleButtons="toggleMobileButtons" :at_source="at_source" :nt_source="nt_source"></ContextButton>
 </template>
 
 <script setup lang="ts">
@@ -31,11 +34,24 @@ const nt_text = ref('');
 const nt_source = ref('');
 const $q = useQuasar()
 
+const toggleMobileButtons = ref(true)
+
 function handleClick() {
-  if ($q.platform.is.mobile && $q.fullscreen.isActive) {
-    $q.fullscreen.exit()
+  if ($q.platform.is.mobile) {
+    toggleMobileButtons.value = !toggleMobileButtons.value
+    $q.fullscreen.isActive && $q.fullscreen.exit()
   }
 }
+
+const secondsSinceMove = ref(0)
+
+function handleMousemove() {
+  secondsSinceMove.value = 0
+}
+
+setInterval(() => {
+  secondsSinceMove.value += 1
+}, 1000)
 
 const store = useDateStore();
 
